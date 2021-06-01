fffed4ea5e7470a8fc935f5f0de1b27e21d40e55
Ankur Shah <ankur@codeplatoon.org>
2021-06-01 09:46:03 -0500

    completed python solution

--- python/balance_parens.py
+++ python/balance_parens.py
@@ -1,2 +1,49 @@
-def balance_parens(str):
-    pass
+def balance_parens(str):    
+    if str == "":
+        return str 
+    
+    # input will be a string with letters, numbers, or parens
+    balanced_str = ""
+    open_parens_index_list = []
+
+    # loop through entire string
+    for i, c in enumerate(str):
+        #print(i)
+
+        # if we get an opening parens.... then we MAY have a closing parens buddy
+        if c == "(":
+            # append index of open parens
+            open_parens_index_list.append(len(balanced_str))
+            #print(balanced_str, open_parens_index_list)
+
+        # if we get a closing parens... check if there was a opening parens buddy
+        elif c == ")":
+            if len(open_parens_index_list) > 0:
+                open_parens_index_list.pop()
+                #print(balanced_str, open_parens_index_list)
+            else:
+                continue
+
+        # append if we get a letter, number, or opening parens
+        balanced_str += c
+
+        # added logic here
+
+    #print("leftover open parens")
+    #print(open_parens_index_list)
+
+    # if there are no unmatched open parens, we're done!
+    if len(open_parens_index_list) == 0:
+        return balanced_str
+
+    fully_balanced_str = ""
+    for i, c in enumerate(balanced_str):
+        if i not in open_parens_index_list:
+            fully_balanced_str += c
+
+    # return value should be a string (balanced)
+    return fully_balanced_str
+
+
+#output = balance_parens("((a(b)c(((") # should return "a(b)c"
+#print(output)
--- python/balance_parens_spec.py
+++ python/balance_parens_spec.py
@@ -1,8 +1,61 @@
 from balance_parens import balance_parens
+import unittest
 
-# Add more test cases!...
-print(balance_parens("abc(d)e(fgh))(i)j)k") == "abc(d)e(fgh)(i)jk")
-print(balance_parens("abc((d)e(fgh)(i)j(k") == "abc(d)e(fgh)(i)jk")
 
-# Challenge: nested parentheses...
-print(balance_parens("abc(d)(ef(g(h))ij)k)lm()o)p") == "abc(d)(ef(g(h))ij)klm()op")
+class BalanceParensTestCases(unittest.TestCase):
+    def test_simple_is_balanced_parens(self):
+        output = balance_parens("()")
+        self.assertEqual(output, "()")
+
+    def test_extra_closing_parens(self):
+        output = balance_parens("a(b)c)") # should return "a(b)c"
+        self.assertEqual(output, "a(b)c")
+
+    def test_extra_closing_parens_2(self):
+        output = balance_parens("(a)(bdd)c)") # should return "(a)(bdd)c"
+        self.assertEqual(output, "(a)(bdd)c")
+
+    def test_case_4(self):
+        output = balance_parens("a(dbvb)c)") # should return "a(dbvb)c"
+        self.assertEqual(output, "a(dbvb)c")
+
+    def test_case_5(self):
+        # empty return
+        output = balance_parens(")(") # should return ""
+        self.assertEqual(output, "")
+
+    def test_case_6(self):
+        output = balance_parens("a(b)(c)())") # should return "a(b)(c)()"
+        self.assertEqual(output, "a(b)(c)()")
+
+    def test_case_7(self):
+        output = balance_parens("(((((") # should return ""
+        self.assertEqual(output, "")
+
+    def test_case_8(self):
+        output = balance_parens(")))))") # should return ""
+        self.assertEqual(output, "")
+
+    def test_case_9(self):
+        output = balance_parens("(((a(b)((c)((") # should return "a(b)c"
+        self.assertEqual(output, "a(b)(c)")
+
+    def test_case_10(self):
+        output = balance_parens(")(())(") # should return "(())"
+        self.assertEqual(output, "(())")
+
+    def test_case_11(self):
+        output = balance_parens("(()()(") # should return "()()"
+        self.assertEqual(output, "()()")
+
+    def test_case_12(self):
+        output = balance_parens(")())(()()(") # should return "()()()"
+        self.assertEqual(output, "()()()")
+
+    def test_case_13(self):
+        output = balance_parens("") # should return ""
+        self.assertEqual(output, "")
+
+
+if __name__ == "__main__":
+    unittest.main()
